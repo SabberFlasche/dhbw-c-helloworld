c05f0fbc064c3619d948c165d5c021613d6a197d
Pascal L <pascal.luebeck@mailbox.org>
2023-10-12 09:30:38 +0200

    tests

--- helloworld.c
+++ helloworld.c
@@ -2,4 +2,22 @@
 
 int main() {
    printf("Hello World!\n");
+   int a;
+   scanf("%d", &a);
+   switch (a){
+      case 0:
+         printf("You chose 0");
+         break;
+      case 1:
+         printf("You chose 1");
+         break;
+      case 2:
+         printf("You chose 2");
+         break;
+      default:
+         printf("Unkown selection!");
+         break;
+   }
+   printf("\n");
+   printf("%d\n", a);
 }
